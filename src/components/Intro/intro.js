06671bed0d2ccb07c71ab9610fc556bbe71b9c84
unknown <RutulBeladia>
2024-06-28 20:53:43 +0200

    react-scroll removed

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './intro.css';
 import bg from '../../assets/image.png';
-import {Link} from 'react-scroll'
 import hireimg from '../../assets/hireme.png'
 const Intro = () => {
   return (
@@ -10,7 +9,7 @@ const Intro = () => {
             <span className='hello'>Hello</span>
             <span className="introtext">I'm<span className='introName'> Rutul</span><br />Software Engineer</span>
             <p className="intropara">Passionate Fullstack Web Developer with expertise in Node.js, Python,Php/MVC and front-end technologies.<br/> Skilled in crafting innovative web applications with robust backends and visually stunning, responsive interfaces.</p>
-            <Link><button className='btn' onClick={()=> document.getElementById('contact').scrollIntoView({behavior: 'smooth'})}><img src={hireimg} alt="Hire Me" className='btnimg'/>Hire Me</button></Link>
+            <button className='btn' onClick={()=> document.getElementById('contact').scrollIntoView({behavior: 'smooth'})}><img src={hireimg} alt="Hire Me" className='btnimg'/>Hire Me</button>
         </div>
         <img src={bg} alt="" className='bg' />
     </section>
